48dada71de1424cdc8e59f33f6be5bb523d22fcc
Zagot-byte <sriraghav006@gmail.com>
2026-02-05 11:43:45 +0530

    Delete Ace-Editor-main directory

--- Ace-Editor-main/background.js
+++
@@ -1,8 +0,0 @@
-console.log("Ace Editor extension background loaded (v1.4).");
-
-// Handle extension installation
-chrome.runtime.onInstalled.addListener(function(details) {
-  if (details.reason === 'install') {
-    console.log('Ace Editor extension installed');
-  }
-});
